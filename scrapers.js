022aa4e47b952a4cbb6b0aa5381343dd8d1c14b0
James Moseley <jamesmoseley@jamess-mbp.lan>
2021-05-19 13:11:21 -0700

    added bodyparser back in

--- scrapers.js
+++ scrapers.js
@@ -2,11 +2,12 @@ const express = require("express");
 const path = require('path');
 const cheerio = require("cheerio");
 const { default: axios } = require("axios");
+var bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
