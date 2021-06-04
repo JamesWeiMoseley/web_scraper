578115d5803457f60bed661132d2a52fc0f0e17d
James Moseley <jamesmoseley@jamess-mbp.lan>
2021-06-03 18:53:46 -0700

    added new features

--- client.js
+++ client.js
@@ -1,28 +1,34 @@
 console.log("client side worked");
-const url = 'https://web-s-server.herokuapp.com/';
-
 
 let output_text = document.getElementById("output");
 let btn = document.getElementById("submit_button");
 let url_box = document.getElementById("url_input");
 let api_box = document.getElementById("api_input");
 let server = "https://web-s-server.herokuapp.com/";
+// let server = "http://localhost:3000/";
 
-btn.addEventListener("click", (e) => {
-    console.log(url_box.value);
-    output_text.textContent = "Loading...";
-    // console.log(api_box.value);
 
-    let config = {
-    headers: {
-        "Content-Type" : "application/json"}, 
-    responseType: "text"
+function findWhichTextToScrape() {
+    var ele = document.getElementsByName("which");
+    let text_type = "";
+    if(ele[0].checked) {
+        text_type = "p";
+    } else if (ele[1].checked){
+        text_type = "h";
+    } else {
+        alert("You must check which text to be scraped");
+        location.reload();
     }
+    return text_type;
+}
 
-    let payload = {
-        url_box : url_box.value, 
-        api_box : api_box.value
-    };
+// will actually send the post to the API
+function axiosPost(payload) {
+    let config = {
+        headers: {
+            "Content-Type" : "application/json"}, 
+        responseType: "text"
+    }
 
     axios.post(server, payload, config)
     .then(res => {
@@ -30,5 +36,27 @@ btn.addEventListener("click", (e) => {
     })
     .catch(error => console.error(error));
 
+}
+
+btn.addEventListener("click", (e) => {
+    output_text.textContent = "Loading...";
+    let text_type = findWhichTextToScrape();
+
+    // setting up the parameters to send 
+    let payload = {
+        url_box : url_box.value, 
+        api_box : api_box.value,
+        text_type: text_type
+    };
+
+    // alert box to protect api from being overloaded. 
+    if(api_box.value==="" || url_box==="") {
+        alert("You are missing values...");
+        location.reload();
+    }
+
+    axiosPost(payload);
     e.preventDefault();
+    
 });
+
